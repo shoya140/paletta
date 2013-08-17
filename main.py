ac45ab44a8ac1b7352fb20a9b9298b025046ccce
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2013-08-17 21:45:55 +0900

    style

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tornado.options
 import tornado.web
 
 from tornado.options import define, options
-define("port", default=8000, help="run on the given port", type=int)
+define("port", default=8010, help="run on the given port", type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
@@ -14,6 +14,7 @@ class IndexHandler(tornado.web.RequestHandler):
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     app = tornado.web.Application(
+        debug = True,
         handlers=[(r'/', IndexHandler)],
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static")
